ded8324de3a0aff39495a531aae8386f67e0df3a
Gustavo Antunes <gustah71@gmail.com>
2025-06-16 16:16:43 -0300

    Adicionados os arquivos inicias de HTML,CSS e JS.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+    ```javascript
+    document.getElementById('saudacaoBtn').addEventListener('click', () => {
+        document.getElementById('mensagemSaudacao').textContent = 'Olá! Bem-vindo ao meu portfólio!';
+    });
+    ```
