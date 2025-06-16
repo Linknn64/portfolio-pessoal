15ea9325111f3fd759ec7e452a0a5588a80eecc6
Gustavo Antunes <gustah71@gmail.com>
2025-06-16 19:24:32 -0300

    Adicionado uma imagem de perfil no "sobre mim", implemetações no contéudo com tabelas e uma nova funcionalidade no "index" utilizando JS.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
     document.getElementById('saudacaoBtn').addEventListener('click', () => {
         document.getElementById('mensagemSaudacao').textContent = 'Olá! Bem-vindo ao meu portfólio!';
     });
+    
+    const saudacaoBtn = document.getElementById('saudacaoBtn');
+    const skillsSection = document.getElementById('skillsSection'); // Nova referência
+    
+    saudacaoBtn.addEventListener('click', () => {
+     skillsSection.style.display = skillsSection.style.display === 'none' ? 'block' : 'none';
+    });
